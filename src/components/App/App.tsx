8093fff5cd64bcfb6f5af682a76e737ab9e5fd84
shekyb <bojana.sekeljic@gmail.com>
2020-07-09 23:52:23 +0200

    adds basic input ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 import useLaunchesByDate from "../../hooks/useLaunchesByDate";
 
+
 const App: React.FC = () => {
-    //2015-08-20/2015-09-20
-    const res: ResponseObject = useLaunchesByDate({ dateFrom: '2020-07-20', dateTo: '2020-12-20' })
+    const [dates, setDates] = useState({
+        dateFrom: moment().format('YYYY-MM-DD'),
+        dateTo: moment().add(3, 'month').format('YYYY-MM-DD'),
+    })
 
+    const [datesToFetch, setDatesToFetch] = useState({...dates})
+    const res: ResponseObject = useLaunchesByDate(datesToFetch);
+    const updateDate = (value: string, prop: string) => {
+        setDates({
+            ...dates,
+            [prop]: value,
+        })
+    }
 
-
+    const getResults = () => {
+        setDatesToFetch(dates);
+    }
     if (res.isLoading) {
         return (<p>loading</p>);
     }
@@ -16,6 +30,18 @@ const App: React.FC = () => {
     if (res.data) {
         return (
             <>
+                <div style={{marginBottom: '10px'}}>
+                    <div style={{marginBottom: '10px'}}>
+                        <label>Enter from date: </label>
+                        <input type="text" value={dates.dateFrom} placeholder="YYYY-MM-DD" onChange={(e) => updateDate(e.target.value, 'dateFrom')} />
+                    </div>
+                    <div style={{marginBottom: '10px'}}>
+                        <label>Enter to date: </label>
+                        <input type="text" value={dates.dateTo} placeholder="YYYY-MM-DD" onChange={(e) => updateDate(e.target.value, 'dateTo')} />
+                    </div>
+                    <button onClick={getResults}>fetch Results</button>
+                </div>
+                <h3>Launches from {dates.dateFrom} to {dates.dateTo}</h3>
                 {res.data.launches.map((launch: Launch) => {
                     return (
                     <div style={{ borderBottom: '1px solid grey'}}>
@@ -29,7 +55,7 @@ const App: React.FC = () => {
                                     <p>name: {pad.name}</p>
                                     <p>lat: {pad.latitude}</p>
                                     <p>long: {pad.longitude}</p>
-                                    <p>agencies: {pad.agencies && pad.agencies.map((agency: Agency) => (<p>{agency.name}</p>))}</p>
+                                    <p>agencies: {pad.agencies && pad.agencies.map((agency: Agency) => (<span>{agency.name}</span>))}</p>
                                 </>)
                             })}
                         </div>
